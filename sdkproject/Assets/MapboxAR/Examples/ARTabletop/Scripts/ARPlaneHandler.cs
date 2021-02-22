9e54d7aedb071a2ae2bbff5f2581f4f8ba36bfcf
lwsoftdean <jungmoona@gmail.com>
2021-02-22 23:52:56 +0900

    -plane error fix

--- sdkproject/Assets/MapboxAR/Examples/ARTabletop/Scripts/ARPlaneHandler.cs
+++ sdkproject/Assets/MapboxAR/Examples/ARTabletop/Scripts/ARPlaneHandler.cs
@@ -27,52 +27,60 @@ namespace UnityARInterface
 				List<ARPlane> addedPlanes = ev.added;
 				if (addedPlanes.Count > 0)
 				{
-					foreach (ARPlane plane in addedPlanes)
+					Debug.Log("Plane Added!!");
+					foreach( var pl in addedPlanes)
 					{
-						GameObject instance = Instantiate(spawnObject, plane.center, plane.transform.rotation);
-						spawnObjects.Add(plane.trackableId, instance);
+						UpdateARPlane(pl);
 					}
+					
 				}
 
-				List<ARPlane> removedPlanes = obj.removed;
+				List<ARPlane> updatedPlanes = ev.updated;
 
-				if (removedPlanes.Count > 0)
+				if (updatedPlanes.Count > 0)
 				{
-					foreach (ARPlane plane in removedPlanes)
+					foreach (var pl in updatedPlanes)
 					{
-						GameObject destoryTarget = spawnObjects[plane.trackableId];
-						Destroy(destoryTarget);
+						UpdateARPlane(pl);
 					}
 				}
+				//{
+				//	foreach (ARPlane plane in removedPlanes)
+				//	{
+				//		GameObject destoryTarget = spawnObjects[plane.trackableId];
+				//		Destroy(destoryTarget);
+				//	}
+				
 
 			};
-			_arPlaneManager.planeAdded += OnPlaneAdded;
-			_arPlaneManager.planeUpdated += OnPlaneUpdated;
+			//_arPlaneManager.planeAdded += OnPlaneAdded;
+			//_arPlaneManager.planeUpdated += OnPlaneUpdated;
 		}
 
-		void OnPlaneAdded(ARPlaneAddedEventArgs eventArgs)
-		{
-			Debug.Log("Plane Added!!");
-			UpdateARPlane(eventArgs.plane);
-		}
+		//void OnPlaneAdded(ARPlaneAddedEventArgs eventArgs)
+		//{
+		//	Debug.Log("Plane Added!!");
+		//	UpdateARPlane(eventArgs.plane);
+		//}
 
-		void OnPlaneUpdated(ARPlaneUpdatedEventArgs eventArgs)
-		{
-			Debug.Log("Plane Updated!!");
-			UpdateARPlane(eventArgs.plane);
-		}
+		//void OnPlaneUpdated(ARPlaneUpdatedEventArgs eventArgs)
+		//{
+		//	Debug.Log("Plane Updated!!");
+		//	UpdateARPlane(eventArgs.plane);
+		//}
 
 		void UpdateARPlane(ARPlane arPlane)
 		{
 
 			if (_didCacheId == false)
 			{
-				_planeId = arPlane.boundedPlane.Id;
+
+				_planeId = arPlane.trackableId; // arPlane.boundedPlane.Id;
 				Debug.Log("Plane Id " + _planeId.ToString());
 				_didCacheId = true;
 			}
 
-			if (arPlane.boundedPlane.Id == _planeId)
+			if (arPlane.trackableId == _planeId)//arPlane.boundedPlane.Id 
 			{
 				_cachedARPlane = arPlane;
 				Debug.Log("Cached Plane " + _planeId.ToString());
